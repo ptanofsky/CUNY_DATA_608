23b4e75bfd9d17602ae6903e430bb7f4276f3cbc
Philip Tanofsky <philiptanofsky@Philips-Air.attlocal.net>
2020-10-18 00:27:42 -0500

    Mod 4 initial checkin

---
+++ module4/app_v13.py
@@ -0,0 +1,44 @@
+# Dash App with state with submit button
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Input(id='input-1-state', type='text', value='Montreal'),
+    dcc.Input(id='input-2-state', type='text', value='Canada'),
+    html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
+    html.Div(id='output-state')
+])
+
+@app.callback(Output('output-state', 'children'),
+              [Input('submit-button-state', 'n_clicks')],
+              [State('input-1-state', 'value'),
+               State('input-2-state', 'value')])
+def update_output(n_clicks, input1, input2):
+    return u'''
+        The Button has been pressed {} times,
+        Input 1 is "{}"
+        and Input 2 is "{}"
+    '''.format(n_clicks, input1, input2)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
+
